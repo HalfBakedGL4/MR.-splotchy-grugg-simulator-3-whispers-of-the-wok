913279a9152b53a53c000874d463145edeba072c
anatolmo <145755035+anatolmo@users.noreply.github.com>
2025-01-23 11:50:03 +0100

    MixedRealityPrototypeV1

---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+   /* List<Vector3> trackingPos = new List<Vector3>();
+    public float velocity = 1000f;
+
+    bool pickedUp = false;
+    GameObject
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        float triggerRight = OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger);
+
+        if (other.gameObject.tag == "Hand" && triggerRight > 0.9f) 
+        {
+            pickedUp = true;
+            parentHand = other.gameObject; //could set parent instead and use joints??
+        }
+    }*/
+}
