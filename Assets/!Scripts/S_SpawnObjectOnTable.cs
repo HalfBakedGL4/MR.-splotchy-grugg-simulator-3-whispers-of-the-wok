120cd4be901b6a5924b0c8ab828e90148628671f
Takotzo <oyvindroberg@hotmail.no>
2025-01-31 16:26:15 +0100

    pickup only close by

--- Assets/!Scripts/S_SpawnObjectOnTable.cs
+++ Assets/!Scripts/S_SpawnObjectOnTable.cs
@@ -6,27 +6,54 @@ using UnityEngine.XR.ARSubsystems;
 public class S_SpawnObjectOnTable : MonoBehaviour
 {
     [SerializeField] private ARPlaneManager planeManager;
-    [SerializeField] private PlaneClassifications classifications;
-    
+    [SerializeField] private PlaneClassification targetClassification;
     [SerializeField] private GameObject spawnObject;
+
+    private Vector3 spawnObjectSize;
+
     private void OnEnable()
     {
         planeManager.planesChanged += PlaceObjectOnPlane;
     }
+
     private void OnDisable()
     {
         planeManager.planesChanged -= PlaceObjectOnPlane;
     }
 
-    private void PlaceObjectOnPlane(ARPlanesChangedEventArgs obj)
-    
+    private void Start()
     {
-        List<ARPlane> newPlane = obj.added;
-        foreach (var item in newPlane)
+        // Ensure we get the bounds of the object properly
+        spawnObjectSize = spawnObject.GetComponentInChildren<Renderer>().bounds.size;
+    }
+
+    private void PlaceObjectOnPlane(ARPlanesChangedEventArgs obj)
+    {
+        List<ARPlane> newPlanes = obj.added;
+
+        foreach (var plane in newPlanes)
         {
-            if (item.classifications == classifications)
+            if (plane.classification == targetClassification)
             {
-                Instantiate(spawnObject, item.transform.position, Quaternion.identity);
+                // Get plane center and size
+                Vector2 planeSize = plane.size;
+
+                int rows = Mathf.FloorToInt(planeSize.y / spawnObjectSize.z);
+                int columns = Mathf.FloorToInt(planeSize.x / spawnObjectSize.x);
+
+                Vector3 planeCenter = plane.transform.position;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        // Calculate position relative to the plane's center
+                        Vector3 localOffset = new Vector3(j * spawnObjectSize.x, 0, i * spawnObjectSize.z);
+                        Vector3 spawnPosition = planeCenter + plane.transform.rotation * localOffset;
+
+                        Instantiate(spawnObject, spawnPosition, plane.transform.rotation);
+                    }
+                }
             }
         }
     }
