0df023607437dbdb30635814fbeade7ac347e2f0
NoaTheSoap <noa.sjoset@gmail.com>
2025-04-04 12:14:30 +0200

    push

--- Assets/!Scripts/S_Hammer.cs
+++ Assets/!Scripts/S_Hammer.cs
@@ -1,16 +1,51 @@
+using Fusion;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class S_Hammer : MonoBehaviour
+public class S_Hammer : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Damage to objects")]
+    [SerializeField] float wallMultiplier;
+    [SerializeField] float enemyMultiplier;
+    [Networked, OnChangedRender(nameof(NetworkUpdateVisuals))]
+    public float charge { get; set; }
+    private Rigidbody rb;
+    NetworkRunner runner;
+    public bool IsLocalNetworkRig => Object.HasInputAuthority;
+
+    //For testing
+    [SerializeField] Image fillImage;
+
+    public override void Spawned()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (!IsLocalNetworkRig) enabled = false;
+
+        runner = FindFirstObjectByType<NetworkRunner>();
+    }
+
+    private void Update()
+    {
+        charge += 1 * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (transform.parent != null)
+        {
+            DealDamage(Mathf.Clamp(charge, 0, 1));
+        }
+    }
+
+    void DealDamage(float charge)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void NetworkUpdateVisuals()
     {
-        
+        fillImage.fillAmount = charge;
+        // Update visuals for all players here
     }
 }
