df0664fa6dd3ea57d21e14694c575e01eab703f1
Iverander <torje@iverander.com>
2025-04-25 12:58:01 +0200

    Update S_World.cs

--- Assets/!Scripts/Other/S_World.cs
+++ Assets/!Scripts/Other/S_World.cs
@@ -1,30 +1,37 @@
+using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class S_World : MonoBehaviour
 {
-    public static int currentFoodAmount { get; private set; }
+    public static List<S_Food> currentFood { get; private set; } = new List<S_Food>();
     public const int maxFood = 10;
 
     /// <summary>
     /// used to instantiate food
     /// </summary>
-    public static object InstantiateFood(Object food, Vector3 position, quaternion rotation)
+    public static S_Food InstantiateFood(S_Food food, Vector3 position, quaternion rotation)
     {
-        if (currentFoodAmount >= maxFood) return null;
+        if (currentFood.Count >= maxFood) return null;
 
-        currentFoodAmount++;
-        return Instantiate(food, position, rotation);
+        S_Food instantiatedFood = Instantiate(food, position, rotation);
+        currentFood.Add(instantiatedFood);
+
+        return instantiatedFood;
     }
     /// <summary>
     /// used to instantiate food
     /// </summary>
-    public static object InstantiateFood(Object food, Transform parent)
+    public static S_Food InstantiateFood(S_Food food, Transform parent)
     {
-        if (currentFoodAmount >= maxFood) return null;
+        S_Food instantiatedFood = InstantiateFood(food, parent.position, parent.rotation);
+        instantiatedFood.transform.parent = parent;
+        instantiatedFood.transform.localPosition = Vector3.zero;
+        instantiatedFood.transform.localEulerAngles = Vector3.zero;
 
-        currentFoodAmount++;
-        return Instantiate(food, parent);
+        return instantiatedFood;
     }
 
     /// <summary>
@@ -32,7 +39,10 @@ public class S_World : MonoBehaviour
     /// </summary>
     public static void DestroyFood(Object food, float timer = 0)
     {
-        Destroy(food, timer);
-        currentFoodAmount--;
+        S_Food toDestroy = food.GetComponent<S_Food>();
+        if (toDestroy == null) return;
+
+        currentFood.Remove(toDestroy);
+        Destroy(toDestroy, timer);
     }
 }
