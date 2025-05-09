bfffcace2504ddfb85cde47894ff9bee6be6505a
Iverander <torje@iverander.com>
2025-05-09 14:30:33 +0200

    things

--- Assets/!Scripts/Cooking/S_Plate.cs
+++ Assets/!Scripts/Cooking/S_Plate.cs
@@ -5,8 +5,16 @@ public class S_Plate : MonoBehaviour
 {
     public S_DishStatus dishStatus;
 
+    GameObject food;
+
     public void PlateFood(SelectEnterEventArgs args)
     {
         dishStatus = args.interactableObject.transform.GetComponent<S_DishStatus>();
+        food = args.interactableObject.transform.gameObject;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(food);
     }
 }
