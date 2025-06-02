40a3cd95af9797543da3af5bdd7d1361a71192ff
Beatriz Bueno <Fbuenoalmagro@gmail.com>
2025-06-02 14:10:03 +0200

    navmesh fix and simplification

--- Assets/!Scripts/CostumerRelated/S_Alien.cs
+++ Assets/!Scripts/CostumerRelated/S_Alien.cs
@@ -1,6 +1,4 @@
 using Fusion;
-using Oculus.Interaction.Surfaces;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -12,58 +10,26 @@ public class S_Alien : NetworkBehaviour
     public float speed = 1;
 
     private List<S_Appliance> appliances;
+    private int applianceIndex = -1;
 
     public override void Spawned()
     {
-        // Start coroutine manually since there's no automatic Start() in Fusion
-        StartCoroutine(InitializeAfterSpawn());
-    }
-
-    private IEnumerator InitializeAfterSpawn()
-    {
         appliances = FindObjectsByType<S_Appliance>(FindObjectsSortMode.None).ToList();
-        if (appliances == null || appliances.Count == 0)
-        {
-            Debug.LogError("[Navmesh] No appliances found in the scene.");
-        }
-        yield return new WaitUntil(() => (GetComponent<NavMeshSurface>() != null && appliances != null));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (appliances != null)
-        {
-            if (appliances.Count == 0)
-            {
-                Debug.LogError("[Navmesh] No appliances found in the scene.");
-                return;
-            }
-            if (agent.isOnNavMesh)
-            {
-                FollowAppliance();
-            }
-            else
-            {
-                Debug.LogError("[Navmesh] Navmesh Agent not attached to Navmesh Surface.");
-            }
-        }
-        else
-        {
-            Debug.LogError("[Navmesh] Appliances list is null or empty.");
-        }
+        Debug.Log("[Navmesh] Appliances found: " + appliances.Count);
+        FollowAppliance();
     }
 
     void FollowAppliance()
     {
-        if (Camera.main == null) return;
-
         if (agent.isOnNavMesh)
         {
+            if (applianceIndex == -1)
+            {
+                applianceIndex = Random.Range(0, appliances.Count - 1);
+            }
             Vector3 targetPosition;
-            int x = Random.Range(0, appliances.Count - 1);
 
-            targetPosition = appliances[x].transform.position;
+            targetPosition = appliances[applianceIndex].transform.position;
 
             agent.SetDestination(targetPosition);
             agent.speed = speed;
