b222872ef90fa20f8861db3e3290c47c723dc44b
NoaTheSoap <noa.sjoset@gmail.com>
2025-06-05 15:22:24 +0200

    WorkingRadio

--- Assets/!Prefabs/S_RadioController.cs
+++ Assets/!Prefabs/S_RadioController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class S_RadioController : NetworkBehaviour
 {
     [SerializeField] FMODUnity.StudioEventEmitter emitter;
-
+    [Networked] public float Volume {  get; set; }
+    [Networked] public float Channel { get; set; }
     public void Start()
     {
         emitter = GetComponent<FMODUnity.StudioEventEmitter>();
@@ -14,15 +15,20 @@ public class S_RadioController : NetworkBehaviour
         emitter.Play();
     }
 
+    public void onChange()
+    {
 
+    }
     public void SetVolume(float volume)
     {
+        Volume = volume;
         emitter.EventInstance.setParameterByName("Volume", Mathf.Clamp(volume, 0.1f, 1));
         
     }
 
     public void SetChannel(float channel)
     {
+        Channel = channel;
         emitter.EventInstance.setParameterByName("Radio", channel);
         Debug.Log("Set radio channel to: " + channel);
     }
