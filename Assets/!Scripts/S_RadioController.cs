1bea2903d056fb320b090900220f113ccf1c00d4
NoaTheSoap <noa.sjoset@gmail.com>
2025-06-05 15:50:50 +0200

    Radio networked

--- Assets/!Scripts/S_RadioController.cs
+++ Assets/!Scripts/S_RadioController.cs
@@ -1,11 +1,13 @@
+using FMOD;
 using Fusion;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class S_RadioController : NetworkBehaviour
 {
     [SerializeField] FMODUnity.StudioEventEmitter emitter;
-    [Networked] public float Volume {  get; set; }
-    [Networked] public float Channel { get; set; }
+    [Networked, OnChangedRender(nameof(onChange))] public float Volume {  get; set; }
+    [Networked, OnChangedRender(nameof(onChange))] public float Channel { get; set; }
     public void Start()
     {
         emitter = GetComponent<FMODUnity.StudioEventEmitter>();
@@ -17,19 +19,19 @@ public class S_RadioController : NetworkBehaviour
 
     public void onChange()
     {
-
+        emitter.EventInstance.setParameterByName("Volume", Mathf.Clamp(Volume, 0.1f, 1));
+        emitter.EventInstance.setParameterByName("Radio", Channel);
     }
     public void SetVolume(float volume)
     {
         Volume = volume;
-        emitter.EventInstance.setParameterByName("Volume", Mathf.Clamp(volume, 0.1f, 1));
+        //emitter.EventInstance.setParameterByName("Volume", Mathf.Clamp(volume, 0.1f, 1));
         
     }
 
     public void SetChannel(float channel)
     {
         Channel = channel;
-        emitter.EventInstance.setParameterByName("Radio", channel);
-        Debug.Log("Set radio channel to: " + channel);
+        //emitter.EventInstance.setParameterByName("Radio", channel);
     }
 }
